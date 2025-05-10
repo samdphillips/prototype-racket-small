543ac1fe6464c10484824970c161f838f86a38dd
Sam Phillips <samdphillips@gmail.com>
2025-05-09 17:48:20 -0700

    Initial commit

---
+++ info.rkt
@@ -0,0 +1,44 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps '("at-exp-lib"
+               "compiler"
+               "data"
+               "db"
+               "draw"
+               "draw-lib"
+               "expeditor"
+               "errortrace"
+               "html"
+               "math"
+               "net"
+               "net-cookies"
+               "option-contract"
+               "parser-tools"
+               "pconvert-lib"
+               "pict"
+               "profile"
+               "r5rs"
+               "r6rs"
+               "racket-doc"
+               "racket-lib"
+               "rackunit"
+               "rackunit-typed"
+               "readline"
+               "sandbox-lib"
+               "sasl"
+               "scribble"
+               "srfi"
+               "string-constants"
+               "syntax-color"
+               "trace"
+               "typed-racket"
+               "typed-racket-more"
+               "unix-socket"
+               "web-server"
+               "xrepl"
+               "ds-store"))
+
+(define license
+  '(Apache-2.0 OR MIT))
